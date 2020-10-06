7fb7090d094c77f2f692fe6cb225da2db3690c62
Jackykit123 <jackykit19970402@gmail.com>
2020-10-06 21:02:09 +0800

    added fake ban command

---
+++ src/command/fakebanall.ts
@@ -0,0 +1,33 @@
+import * as Discord from 'discord.js';
+
+export default async function banAll(message: Discord.Message): Promise<void> {
+    const { channel, guild, member } = message;
+    if (!guild || !member?.hasPermission('ADMINISTRATOR')) {
+        await channel.send(
+            'This command is only available as `ADMINISTRATOR` in server chat.'
+        );
+        return;
+    }
+    const list = guild.members.cache.map(
+        member => `\`${member.user.username}#${member.user.discriminator}\``
+    );
+    const sentMessage = await channel.send('Banning all members: ...');
+    let i = 0;
+    const timeout = setInterval(async () => {
+        if (i > list.length) {
+            clearInterval(timeout);
+            await channel.send(
+                `Banning all members: ... \nBanned: ${list.join(
+                    ' '
+                )}\nAll members are successfully banned.`
+            );
+            return;
+        }
+        await sentMessage.edit(
+            `Banning all members: ... \nBanned: ${list
+                .filter((_, j) => j <= i)
+                .join(' ')}`
+        );
+        i += 1;
+    }, 1000);
+}
