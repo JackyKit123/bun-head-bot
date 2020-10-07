8b392c079ad5691ae896f2f066fd19b1bfdda849
Jackykit123 <jackykit19970402@gmail.com>
2020-10-07 21:41:06 +0800

    added more question phrase starting phrase to regex

--- src/other/8ball.ts
+++ src/other/8ball.ts
@@ -5,7 +5,7 @@ export default async function eightball(
 ): Promise<void> {
     const { channel, content } = message;
 
-    const regex = /^(does|do|did|wanna|should|is|are|am|should|may|shall|was|were|will|can|could|would|don't|wouldn't|isn't|aren't|ain't|shouldn't|won't|can't)( .+){1,}\?/i;
+    const regex = /^(does|do|did|wanna|should|is|are|am|should|may|shall|was|were|will|can|could|would|have|had|havn't|hadn't|haven't|didn't|don't|wouldn't|isn't|aren't|ain't|shouldn't|won't|can't|weren't|couldn't)( .+){1,}\?/i;
     if (content.match(regex)) {
         const eightballAns = [
             'It is certain',
