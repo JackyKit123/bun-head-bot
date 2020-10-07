c0218b6a91c52c4497f4fff1cf84937f6900d6c0
Jackykit123 <jackykit19970402@gmail.com>
2020-10-08 00:01:08 +0800

    fixing no image found response

--- src/command/giphy.ts
+++ src/command/giphy.ts
@@ -5,28 +5,44 @@ export default async function giphy(message: Discord.Message): Promise<void> {
     const { content, channel, author } = message;
     const searchString = content
         .replace(/[^\040-\176\200-\377]/gi, '')
-        .replace('-usagi giphy ', '');
-    if (!searchString) {
-        await channel.send(
-            'You need to specify a search string as in `-usagi gif <search target>`'
-        );
-        return;
-    }
+        .replace(/^\\?-usagi giphy ?/, '');
 
-    const { data } = await axios.get(
+    let { data } = await axios.get(
         `https://api.giphy.com/v1/gifs/search?api_key=${
             process.env.GIPHY_API_KEY
-        }&q=${encodeURI(searchString)}&limit=1&lang=en`
+        }&q=${encodeURI(searchString)}&lang=en`
     );
-    const { url } = data.data?.[0]?.images?.original;
-    await channel.send(
-        new Discord.MessageEmbed()
-            .setColor('#34ebb4')
-            .setTitle(searchString)
-            .setImage(url)
-            .setFooter(
-                `Requested by ${author.username}#${author.discriminator}`,
-                author.displayAvatarURL()
+    let results = data.data;
+    const embed = new Discord.MessageEmbed()
+        .setColor('#34ebb4')
+        .setFooter(
+            `Requested by ${author.username}#${author.discriminator}`,
+            author.displayAvatarURL()
+        );
+    if (!results.length) {
+        data = (
+            await axios.get(
+                `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&lang=en`
             )
-    );
+        ).data;
+        results = data.data;
+        const randomGif = results;
+        const { id } = randomGif;
+        await channel.send(
+            searchString
+                ? `Cannot find a gif for \`${searchString}\`, how about we look at a random one?`
+                : undefined,
+            embed
+                .setTitle('Random Gif')
+                .setImage(`https://media1.giphy.com/media/${id}/giphy.gif`)
+        );
+    } else {
+        const randomGif = results[Math.floor(Math.random() * results.length)];
+        const { id } = randomGif;
+        await channel.send(
+            embed
+                .setTitle(searchString)
+                .setImage(`https://media1.giphy.com/media/${id}/giphy.gif`)
+        );
+    }
 }
